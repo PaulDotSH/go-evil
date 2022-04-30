9d3d8b94d5d5e02f1d10467c9ed9e08f935e4222
PaulDotSH <paulandreiabrudan@pm.me>
2022-04-30 20:37:16 +0300

    panel design start

---
+++ panel/frontend/js/installer.js
@@ -0,0 +1,22 @@
+document.getElementById('start-install-btn').addEventListener("click", () => {
+    document.getElementById('start-container').classList.add('fadeOut')
+    setTimeout(() => {
+        document.getElementById('start-container').style.display = 'none';
+        document.getElementById('settings-container').classList.add('fadeIn')
+        document.getElementById('settings-container').classList.remove('d-none');
+    
+    }, 1500);
+});
+
+
+Promise.all(
+    document.getElementById('installer-description').getAnimations().map(
+      function(animation) {
+        return animation.finished
+      }
+    )
+  ).then(
+    function() {
+      return document.getElementById('start-install-btn').classList.add('slideLeft');
+    }
+  );
