95c18dbaa0d231dc4ea79be2820a69712019b4df
PaulDotSH <paulandreiabrudan@protonmail.com>
2022-05-03 05:26:15 -0500

    note

--- panel/backend/parser/parser.go
+++ panel/backend/parser/parser.go
@@ -17,7 +17,7 @@ type Setting struct {
 
 //This function will parse a settings file from go-evil, to be used with the panel
 func Parse(path string) []Setting {
-	var ret = make([]Setting, 0, 50)
+	var ret = make([]Setting, 0, 50) //you can change this to improve performance if you modified the code to add more settings, however it isn't going to be a big difference anyway
 
 	file, err := os.Open(path)
 	if err != nil {
@@ -61,7 +61,10 @@ func Parse(path string) []Setting {
 	return ret
 }
 
-//TODO: Make a function that translates a Setting to the apropriate html render
+//TODO: Make a function that translates a Setting to the appropriate html render
+//Use an io.Writer and pass it to any sub-func that would need to change the final html page.
+//Have the /builder endpoint, if the request is get server the webpage, if the request is post, use js or html to make a post request with the settings
+//that need to be applied
 
 func strSliceToStr(slice []string, sep string) string {
 	var ret strings.Builder
